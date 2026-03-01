be97afdd7a75d8735bdfd4a5e2398bf56e3d888c
ajaykalshetty17 <ajaykalshetty17@gmail.com>
2026-03-01 22:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,8 @@ def update_status():
     return redirect("/admin")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
